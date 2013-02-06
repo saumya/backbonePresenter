251ca1ae884549ffcda67fe6a8a2aaac8b43050e
saumya <saumya>
2013-02-06 19:22:14 +0530

    updated collection with three slide data

--- src/modules/collection/slidesCollection.js
+++ src/modules/collection/slidesCollection.js
@@ -10,10 +10,29 @@ function(SlideModel){
 		initialize: function(){
 			console.log('SlidesCollection : initialize : ');
 			//initialise the collection with models
-			var page1=new SlideModel({pageNumber:1,description:'First page'});
-			
+			var page1=new SlideModel({
+				pageNumber:1,
+				description:'First page'
+				});
+			var page2=new SlideModel({
+				pageNumber:2,
+				description:'Second page'
+				});
+			var page3=new SlideModel({
+				pageNumber:3,
+				description:'Third page'
+				});
+			this.add([page1,page2,page3]);
 			//event handlers
-			this.on('add', this.onCollectionChange,this);
+			this.on('add', this.onModelAdd,this);
+			this.on('remove', this.onModelRemove,this);
+			this.on('change', this.onCollectionChange,this);
+		},
+		onModelAdd: function(ev){
+			console.log('SlidesCollection : onModelAdded : ');
+		},
+		onModelRemove: function(ev){
+			console.log('SlidesCollection : onModelRemove : ');
 		},
 		onCollectionChange: function(ev){
 			console.log('SlidesCollection : onCollectionChange : ');

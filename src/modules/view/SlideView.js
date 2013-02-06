404afa56fa8041f675116e6fe4dda6fc92f7f4aa
saumya <saumya>
2013-02-06 15:11:46 +0530

    initialized a template

--- src/modules/view/SlideView.js
+++ src/modules/view/SlideView.js
@@ -14,7 +14,7 @@ function(){
 		render: function(){
 			console.log('SlideView : render : ');
 			//this.$el.clear();
-			this.$el.append(' : Hello World');
+			//this.$el.append(' : Hello World');
 		}
 	});
 	return SlideView;

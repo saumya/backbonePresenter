ad8c4ce3a77d412db95c53aac36194ef96ac5031
saumya <saumya>
2013-02-06 14:44:53 +0530

    added a basic view

--- src/modules/view/SlideView.js
+++ src/modules/view/SlideView.js
@@ -9,6 +9,12 @@ function(){
 		initialize: function(){
 			console.log('SlideView : initialize : ');
 			console.log(this.el);
+			this.render();
+		},
+		render: function(){
+			console.log('SlideView : render : ');
+			//this.$el.clear();
+			this.$el.append(' : Hello World');
 		}
 	});
 	return SlideView;

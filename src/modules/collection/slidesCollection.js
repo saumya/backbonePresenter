659bdc0c2056ee6f700d79dc269229bc5b4730dc
saumya <saumya>
2013-02-06 11:50:42 +0530

    Collection basic implementation is done

--- src/modules/collection/slidesCollection.js
+++ src/modules/collection/slidesCollection.js
@@ -5,13 +5,23 @@
  * Complete :
  * define('modules/collection/SlidesCollection',
  * ['modules/model/SlidesModel'],
- * function(){
+ * function(SlidesModel){
  * 	var SlidesCollection = Backbone.Collection.extend({});
  * });
  * 
  */
 define('modules/collection/SlidesCollection',
 ['modules/model/SlidesModel'],
-function(){
-	var SlidesCollection = Backbone.Collection.extend({});
+function(SlidesModel){
+	var SlidesCollection = Backbone.Collection.extend({
+		model: SlidesModel, //localStorage: new Store('todos-backbone'),  
+		initialize: function(){
+			console.log('SlidesCollection : initialize : ');
+			//event handlers
+			this.on('add', this.onCollectionChange,this);
+		},
+		onCollectionChange: function(ev){
+			console.log('SlidesCollection : onCollectionChange : ');
+		}
+	});
 });

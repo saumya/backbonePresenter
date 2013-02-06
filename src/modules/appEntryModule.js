03187fe82ec64704231bfa665f066691f44e7635
saumya <saumya>
2013-02-06 19:45:55 +0530

    rendeing is done

--- src/modules/appEntryModule.js
+++ src/modules/appEntryModule.js
@@ -26,25 +26,47 @@ define('modules/appEntryModule',
 		var ApplicationEntry = Backbone.View.extend({
 			initialize:function(){
 				console.log('ApplicationEntry : initialize');
-				this.slideModel=new SlideModel();
+				this.pageCounter=0;
 				//collection
-				var slidesCollection=new SlidesCollection();
+				this.slidesCollection=new SlidesCollection();
+				//console.log(slidesCollection.length);
+				this.slideModel=this.slidesCollection.at(0);
 				//view
-				var slide=new SlideView();
-				slide.setData(this.slideModel);
+				this.slide=new SlideView();
+				this.slide.setData(this.slideModel);
 				//event listeners
-				slide.on('SlideView.onBack',this.onBack,this);
-				slide.on('SlideView.onNext',this.onNext,this);
-				slide.listenTo(this.slideModel, 'change', slide.render);
+				this.slide.on('SlideView.onBack',this.onBack,this);
+				this.slide.on('SlideView.onNext',this.onNext,this);
+				//slide.listenTo(this.slideModel, 'change', slide.render);
 				//change model
 				//slideModel.set('pageTitle','Hello');
 			},
 			onNext:function(event){
 				console.log('ApplicationEntry : onNext');
-				this.slideModel.set('pageTitle','Hello');
+				//this.slideModel.set('pageTitle','Hello');
+				//var model=this.slidesCollection.at(1);
+				//this.slideModel.set(model);
+				
+				if(this.pageCounter<=(this.slidesCollection.length-2))
+				{
+					this.pageCounter++;
+					this.slideModel=this.slidesCollection.at(this.pageCounter);
+					this.slide.setData(this.slideModel);
+				}else{
+					console.log('Last page is reached.');
+				}
 			},
 			onBack:function(event){
 				console.log('ApplicationEntry : onBack');
+				
+				if(this.pageCounter>=1)
+				{
+					this.pageCounter--;
+					this.slideModel=this.slidesCollection.at(this.pageCounter);
+					this.slide.setData(this.slideModel);
+				}else{
+					console.log('First page is reached.');
+				}
 			}
 		});
 		return ApplicationEntry;
